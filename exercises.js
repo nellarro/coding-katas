ebd8fbcb726e528a99c2041fb2585222c24b40a0
Jenell Pizarro <nellarro@gmail.com>
2016-07-27 14:46:57 -0400

    halfway

---
+++ exercises.js
@@ -0,0 +1,39 @@
+/**
+ * For each of the following Array methods,
+
+ * 1. use them in an example
+ * 2. use console.log() to make the code run in the console!
+
+ **/
+
+ let letters = ['a', 'b', 'c', 'd', 'e', 'j', 'n', 'l', 'l']
+ let numbers = [1, 2, 3, 4, 5]
+
+ .sort(letters)
+ console.log(letters)
+
+ let greet = 'Hello, my darling'
+ console.log(greet.concat(' ' + 'Jenell' + '!'))
+
+ console.log(numbers.indexOf(4))
+
+ console.log(greet.split(''))
+
+ letters = letters.join()
+ console.log(letters)
+
+// .pop()
+
+// .push()
+
+// .slice()
+
+// .splice()
+
+// .shift()
+
+// .unshift()
+
+// .filter()
+
+// .map()
